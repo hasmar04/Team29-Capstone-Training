fd0866bdbdfdf96e6205cc8515fd8d90276d77cd
Sqwuib <106869628+Sqwuib@users.noreply.github.com>
2026-05-14 15:15:13 +1000

    Refactor model-trainer.py to streamline imports
    Removed installation commands and Roboflow initialization.

--- model-trainer.py
+++ model-trainer.py
@@ -1,17 +1,10 @@
-!pip install roboflow ultralytics
+#######################################
 
-from roboflow import Roboflow
+  #PASTE YOUR ROBOFLOW SNIPPET HERE#
+
+#######################################
+
 from ultralytics import YOLO
-
-# init roboflow
-rf = Roboflow(api_key="INSERT YOUR API KEY")
-
-project = rf.workspace("WORKSPACE").project("PROJECT")
-
-dataset = project.version(1).download("yolov8") 
-
-print(dataset.location)
-
 # keep your model
 model = YOLO("yolo26m.pt")
 
